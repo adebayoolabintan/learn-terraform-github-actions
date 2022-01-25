82a5feb93638dea4809d28664841d161993b4ae6
olaadejnr <olaadejnr@yahoo.com>
2022-01-25 22:49:43 +0000

    changed the resource name from web to web01

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ provider "aws" {
 
 resource "random_pet" "sg" {}
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web01" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
@@ -52,5 +52,5 @@ resource "aws_security_group" "web-sg" {
 }
 
 output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+  value = "${aws_instance.web01.public_dns}:8080"
 }
